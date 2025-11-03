155f9af3031ac48d05a9fb7247c3082abdef51aa
Turculeț Alexandru-Ioan <36742066+Androidus2@users.noreply.github.com>
2025-11-03 13:51:15 +0200

    Add basic attack system and a dummy (#2)

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack : MonoBehaviour
+{
+    InputAction attackAction;
+    float attackValue;
+
+    [SerializeField]
+    float attackDamage;
+
+    [SerializeField]
+    float attackCooldown;
+
+    float timeSinceLastAttack;
+
+    List<EnemyHealth> targets;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        attackAction = InputSystem.actions.FindAction("Attack");
+
+        timeSinceLastAttack = attackCooldown;
+
+        targets = new List<EnemyHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attackValue = attackAction.ReadValue<float>();
+        timeSinceLastAttack += Time.deltaTime;
+        if(attackValue > 0f && timeSinceLastAttack >= attackCooldown)
+        {
+            timeSinceLastAttack = 0f;
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        if (targets.Count > 0)
+        {
+            EnemyHealth hitEnemy = targets[0];
+            hitEnemy.TakeDamage(attackDamage);
+            if (hitEnemy.IsDead())
+                targets.RemoveAt(0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyHealth enemy = other.GetComponent<EnemyHealth>();
+            if (enemy == null)
+            {
+                Debug.LogError("Found an enemy without an EnemyHealth component!");
+                return;
+            }
+
+            targets.Add(enemy);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyHealth enemy = other.GetComponent<EnemyHealth>();
+            if (enemy == null)
+            {
+                Debug.LogError("Found an enemy without an EnemyHealth component!");
+                return;
+            }
+
+            targets.Remove(enemy);
+        }
+    }
+}
