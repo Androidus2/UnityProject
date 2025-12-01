d0342b557d9fd81e7cc64cfe5cb59916cb2a9961
Androidus2 <alexandruturculet@ymail.com>
2025-12-01 16:52:01 +0200

    Add tween to inventory pop up and item pick up

--- Assets/Scripts/InventorySystem/Inventory/DisplayInventory.cs
+++ Assets/Scripts/InventorySystem/Inventory/DisplayInventory.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -22,6 +23,9 @@ public class DisplayInventory : MonoBehaviour
     
     [SerializeField]
     private int numberOfColumns;
+
+    [SerializeField]
+    private float itemScaleDuration = 0.15f;
 
 
     Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
@@ -55,6 +59,22 @@ public class DisplayInventory : MonoBehaviour
         return new Vector3(xStart + (xSpaceBetweenItems * (i % numberOfColumns)), yStart + (-ySpaceBetweenItems * (i/numberOfColumns)), 0f);
     }
 
+    void CreateNewItemEntry(InventorySlot slot, int index)
+    {
+        var obj = Instantiate(slot.GetItem().GetIcon(), Vector3.zero, Quaternion.identity, transform);
+
+        RectTransform rect = obj.GetComponent<RectTransform>();
+        rect.localPosition = GetPosition(index);
+        obj.GetComponentInChildren<TextMeshProUGUI>().text = slot.GetItem().name;
+
+        itemsDisplayed.Add(slot, obj);
+
+        // Add tween to make it smoothly pop up
+        rect.localScale = Vector3.zero;
+        rect.DOScale(Vector3.one, itemScaleDuration).SetEase(Ease.OutBack, 1.3f);
+    }
+
+
     public void UpdateDisplay()// TO DO - instead of checking every frame of the inventory has been updated,
                                     //have the 'owner' of the inventory call to update the display to add or remove an item
     {
@@ -62,10 +82,7 @@ public class DisplayInventory : MonoBehaviour
         {
             if (!itemsDisplayed.ContainsKey(inventory.GetItems(i)))
             {
-                var obj = Instantiate(inventory.GetItems(i).GetItem().GetIcon(), Vector3.zero, Quaternion.identity, transform);
-                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
-                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.GetItems(i).GetItem().name;
-                itemsDisplayed.Add(inventory.GetItems(i), obj);
+                CreateNewItemEntry(inventory.GetItems(i), i);
             }
         }
     }
--- Assets/Scripts/InventorySystem/Inventory/PopUpPanel.cs
+++ Assets/Scripts/InventorySystem/Inventory/PopUpPanel.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using DG.Tweening;
 
 public class PopUpPanel : MonoBehaviour
 {
     [SerializeField]
     private GameObject Panel;
     InputAction inventoryButton;
+
+    Tweener scaleTween;
+    [SerializeField]
+    private float scaleDuration = 0.15f;
 
     void Awake()
     {
@@ -25,6 +30,30 @@ public class PopUpPanel : MonoBehaviour
 
     void TogglePanel()
     {
-        Panel.SetActive(!Panel.activeSelf);
+        bool shouldOpen = !Panel.activeSelf;
+
+        // Kill any ongoing tween so spamming doesn't break it
+        scaleTween?.Kill();
+
+        if (shouldOpen)
+        {
+            // Ensure panel is visible before animation
+            Panel.SetActive(true);
+            Panel.transform.localScale = Vector3.zero;
+
+            scaleTween = Panel.transform
+                .DOScale(Vector3.one, scaleDuration)
+                .SetEase(Ease.OutBack, 1.4f);
+        }
+        else
+        {
+            scaleTween = Panel.transform
+                .DOScale(Vector3.zero, scaleDuration)
+                .SetEase(Ease.InBack, 1.2f)
+                .OnComplete(() =>
+                {
+                    Panel.SetActive(false);
+                });
+        }
     }
 }
